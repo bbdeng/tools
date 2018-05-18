76c4692ec1fead2387e3e4e26cdb3ed8e5c46f7d
邓帮办 <www.denbangban@qq.com>
2018-05-18 17:49:34 +0800

    添加笔记

--- tools.js
+++ tools.js
@@ -1,32 +1,136 @@
-//读取当前样式的方法
-//obj需要获取那个对象
-//name需要获取该对象的样式名字		
-function getStyle(obj, name) {
-    //获取样式的2种方法
-    //1.getComputedStyle(①,②)["样式名"];
-    //该方法适用于正常的浏览器并且是window的方法可以直接使用
-    //需要2个参数，①为需要获取样式的元素，②可以传递一个伪元素，一般都未null
-    //该方法返回一个对象，对象其中包含了当前元素的样式，需要获取那个样式需要在后面添加  .样式名
-    //和下面方法的区别：如果元素没有设置默认样式，获取到的是真实的值，而不是默认的样式
-    //2.元素.currentStyle.样式名
-    //该方法用来读取当前正在显示的样式，注意该方法如果当前元素没有设置样式，则获取他的默认值
-    //cunrrentStyle这个方法只有在IE中支持
-    var val = null , reg = null ;
-    if (window.getComputedStyle) { //这里的window必须写 因为在IE8中没有该对象，未被定义是一个变量，变量没有会报错，属性没有只会返回undefined
-        val = getComputedStyle(obj, null)[name]; //这里的.name不行，因为.name表示获取到.name样式，写死了
-    } else { //这里是IE6-8
-        //对透明度进行特殊处理,IE6-8下透明度是这么设置的filter:alpha(opacity=50);  值在0-100
-        if (name === "opacity"){
-            val = obj.currentStyle.filter; //"alpha(opacity=50)" 需要对结果进行提取，然后在除去100才和标准浏览器相同
-            reg = /^alpha\(opacity=(\d+(?:\.\d+)?)\)$/i;
-            val = reg.test(val) ? reg.exec(val)[1]/100 : 1;
-        }else {
-            val = obj.currentStyle[name];
+var tools = (function () {
+    //读取当前样式的方法
+    //obj需要获取那个对象
+    //name需要获取该对象的样式名字
+    function getStyle(obj, name) {
+        //获取样式的2种方法
+        //1.getComputedStyle(①,②)["样式名"];
+        //该方法适用于正常的浏览器并且是window的方法可以直接使用
+        //需要2个参数，①为需要获取样式的元素，②可以传递一个伪元素，一般都未null
+        //该方法返回一个对象，对象其中包含了当前元素的样式，需要获取那个样式需要在后面添加  .样式名
+        //和下面方法的区别：如果元素没有设置默认样式，获取到的是真实的值，而不是默认的样式
+        //2.元素.currentStyle.样式名
+        //该方法用来读取当前正在显示的样式，注意该方法如果当前元素没有设置样式，则获取他的默认值
+        //cunrrentStyle这个方法只有在IE中支持
+        var val = null , reg = null ;
+        if (window.getComputedStyle) { //这里的window必须写 因为在IE8中没有该对象，未被定义是一个变量，变量没有会报错，属性没有只会返回undefined
+            val = getComputedStyle(obj, null)[name]; //这里的.name不行，因为.name表示获取到.name样式，写死了
+        } else { //这里是IE6-8
+            //对透明度进行特殊处理,IE6-8下透明度是这么设置的filter:alpha(opacity=50);  值在0-100
+            if (name === "opacity"){
+                val = obj.currentStyle.filter; //"alpha(opacity=50)" 需要对结果进行提取，然后在除去100才和标准浏览器相同
+                reg = /^alpha\(opacity=(\d+(?:\.\d+)?)\)$/i;
+                val = reg.test(val) ? reg.exec(val)[1]/100 : 1;
+            }else {
+                val = obj.currentStyle[name];
+            }
+        }
+        reg = /^(-?\d+(\.\d+)?)(px|pt|rem|em)?$/i;//去除单位的正则
+        return reg.test(val) ? parseFloat(val) :val ;
+    };
+
+    //写一个方法来操作
+    //兼容各浏览器，获取浏览器本身的盒子模型
+    //传入第1个参数表示获取，2个参数同时传入表示设置
+    function win(attr,value) {
+        if (typeof value === "undefined"){//没有传递value值,表示获取
+            return document.documentElement[attr]||document.body[attr];
+        }
+        //设置
+        document.documentElement[attr] = value;
+        document.body[attr] = value;
+    }
+
+    //写一个offset方法来获取页面任意元素相对页面的水平偏移和垂直偏移
+    function offset(currentElement) {
+        if (typeof currentElement === "undefined") {
+            console.log("offset必须输入参数!");
+            return;
+        }
+        var totalLeft = null , totalTop = null , parent = currentElement.offsetParent;
+        //首先把自己本身的偏移量进行加
+        totalLeft += currentElement.offsetLeft;
+        totalTop += currentElement.offsetTop;
+        while (parent){//只要没有找到body，我们就把父级参照物的边框和偏移也进行叠加
+            if (navigator.userAgent.indexOf("MSIE 8.0") === -1){//不是标准的IE8浏览器，才进行累加，IE8本身已经算了
+                //加父级元素的边框
+                totalLeft += parent.clientLeft ;
+                totalTop += parent.clientTop;
+            }
+            //加父级元素的偏移
+            totalLeft += parent.offsetLeft;
+            totalTop += parent.offsetTop;
+            parent = parent.offsetParent;
+        }
+        return {left:totalLeft,top:totalTop};
+    }
+
+    //多事件的绑定
+    function bind(obj , eventStr , callback){
+        if(obj.addEventListener){
+            //大部分浏览器兼容的方式
+            obj.addEventListener(eventStr , callback , false);
+        }else{
+            /*
+             * this是谁由调用方式决定
+             * callback.call(obj)
+             */
+            //IE8及以下
+            obj.attachEvent("on"+eventStr , function(){
+                //在匿名函数中调用回调函数
+                callback.call(obj);
+            });
         }
     }
-    reg = /^(-?\d+(\.\d+)?)(px|pt|rem|em)?$/i;//去除单位的正则
-    return reg.test(val) ? parseFloat(val) :val ;
-};
+
+    //写一个兼容各个浏览器的创建ajax对象的方法
+    function createXHR() {
+        var xhr = null;
+        var ary = [
+            function () {
+                return new XMLHttpRequest();
+            },
+            function () {
+                return new ActiveXObject("Microsoft.XMLHTTP");
+            },
+            function () {
+                return new ActiveXObject("Msxml1.XMLHTTP");
+            },
+            function () {
+                return new ActiveXObject("Msxml3.XMLHTTP");
+            }
+        ];
+        var flag = false; //定义一个flag标识默认改方法中没有可以兼容您浏览器的方法
+        for (var i=0,len = ary.length; i<len ; i++){
+            var curFn = ary[i];
+            try {
+                xhr = curFn();
+                //本次循环获取的方法执行没有出现错误：说明该方法就是我们想要的方法，我们在下一次直接执行这个小方法即可，需要从新把createXHR重写成改循环下的方法，然后直接退出循环
+                createXHR = curFn;
+                flag = true; //找到了改方法将flag标识改为true。标识所写函数中有方法可以创建ajax对象来兼容您的浏览器
+                break; //找到该方法后结束这个循环
+            }catch (e){
+                //这里面标识上面的方法报错会进入这里，不写东西 直接执行下一次循环
+            }
+        }
+        if (!flag){
+            //如果循环了上面所有的方法都报错，只能没有说明该方法中没有可以兼容您的浏览器，抛出一个错误
+            throw new Error ("your browser is not support ajax,please change your browser , try again!");
+        }
+
+        return xhr;
+    }
+
+    return {
+        getStyle:getStyle,
+        win:win,
+        offset:offset,
+        bind:bind,
+        createXHR:createXHR
+    };
+
+})();
+
 
 
 //创建一个执行简单动画的函数F
@@ -144,58 +248,11 @@ function CreateLikeArray() {
 CreateLikeArray.prototype = new Array;
 CreateLikeArray.prototype.constructor = CreateLikeArray;
 
-//写一个方法来操作兼容各浏览器，获取浏览器本身的盒子模型
-//传入第1个参数表示获取，2个参数同时传入表示设置
-function win(attr,value) {
-    if (typeof value === "undefined"){//没有传递value值,表示获取
-        return document.documentElement[attr]||document.body[attr];
-    }
-    //设置
-    document.documentElement[attr] = value;
-    document.body[attr] = value;
-}
 
 
-//写一个offset方法来获取页面任意元素相对页面的水平偏移和垂直偏移
-function offset(currentElement) {
-    if (typeof currentElement === "undefined") {
-        console.log("offset必须输入参数!");
-        return;
-    }
-    var totalLeft = null , totalTop = null , parent = currentElement.offsetParent;
-    //首先把自己本身的偏移量进行加
-    totalLeft += currentElement.offsetLeft;
-    totalTop += currentElement.offsetTop;
-    while (parent){//只要没有找到body，我们就把父级参照物的边框和偏移也进行叠加
-        if (navigator.userAgent.indexOf("MSIE 8.0") === -1){//不是标准的IE8浏览器，才进行累加，IE8本身已经算了
-            //加父级元素的边框
-            totalLeft += parent.clientLeft ;
-            totalTop += parent.clientTop;
-        }
-        //加父级元素的偏移
-        totalLeft += parent.offsetLeft;
-        totalTop += parent.offsetTop;
-        parent = parent.offsetParent;
-    }
-    return {left:totalLeft,top:totalTop};
-}
 
-//多事件的绑定
-function bind(obj , eventStr , callback){
-    if(obj.addEventListener){
-        //大部分浏览器兼容的方式
-        obj.addEventListener(eventStr , callback , false);
-    }else{
-        /*
-         * this是谁由调用方式决定
-         * callback.call(obj)
-         */
-        //IE8及以下
-        obj.attachEvent("on"+eventStr , function(){
-            //在匿名函数中调用回调函数
-            callback.call(obj);
-        });
-    }
-}
+
+
+
 
 
